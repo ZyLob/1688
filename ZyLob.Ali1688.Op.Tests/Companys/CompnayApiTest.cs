07f932182381d176c2ca1ac5906a64805d40bc64
状元实验室 <2387979592@qq.com>
2015-08-11 15:52:50 +0800

    实例模型建设

---
+++ ZyLob.Ali1688.Op.Tests/Companys/CompnayApiTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZyLob.Ali1688.Op.Companys;
+
+namespace ZyLob.Ali1688.Op.Tests.Companys
+{
+    [TestClass]
+    public class CompnayApiTest
+    {
+        [TestMethod]
+        public void GetAliCompanyInfoTest()
+        {
+          var compnay=  CompnayApi.GetAliCompanyInfo("", "b2b-2080249682");
+            var i = 1;
+        }
+    }
+}
