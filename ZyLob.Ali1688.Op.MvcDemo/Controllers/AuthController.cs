cc77aad10de82317231666b108ec9dd55f859dff
状元实验室 <2387979592@qq.com>
2015-08-13 10:22:14 +0800

    编写部分授权接口和产品接口在mvc使用案例

---
+++ ZyLob.Ali1688.Op.MvcDemo/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using ZyLob.Ali1688.Op.Authorization;
+
+namespace ZyLob.Ali1688.Op.MvcDemo.Controllers
+{
+    /// <summary>
+    /// 授权demo控制器
+    /// </summary>
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 阿里登录回调地址
+        /// </summary>
+        public JsonResult Login(string code)
+        {
+            //获取登录口令
+            var token = AuthApi.GetAliToken(code);
+           /*******
+            * 
+            * 登录操作并记录相关口令信息
+            * 
+            * *****/
+            return Json("登录成功",JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ ZyLob.Ali1688.Op.MvcDemo/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZyLob.Ali1688.Op.Models;
+using ZyLob.Ali1688.Op.Product;
+
+namespace ZyLob.Ali1688.Op.MvcDemo.Controllers
+{
+    /// <summary>
+    /// 产品Demo控制器
+    /// </summary>
+    public class ProductController : Controller
+    {
+      
+        /// <summary>
+        /// 产品搜索
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProductList()
+        {
+            //产品搜索
+            var products = ProductApi.ProductSeach(new ProductSeachModel
+            {
+                MemberId = "szhf0"
+            });
+            var pageSize = products.PageSize;
+            return View();
+        }
+        /// <summary>
+        /// 获取单个产品
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProductOne()
+        {
+            var product = ProductApi.GetProductByOfferId("899955287");
+            return View();
+        }
+
+    }
+}
