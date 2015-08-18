9c7017a696d2fbb9f662e8477436532b99e6feb1
状元实验室 <2387979592@qq.com>
2015-08-18 13:16:42 +0800

    支持yyyy-MM-dd时间格式转换

--- ZyLob.Ali1688.Op/Common/DateTimeExpression.cs
+++ ZyLob.Ali1688.Op/Common/DateTimeExpression.cs
@@ -44,7 +44,15 @@ namespace ZyLob.Ali1688.Op.Common
             {
                 return DateTime.MinValue;
             }
-            return DateTime.ParseExact(aliDateStr.Substring(0, 14), "yyyyMMddHHmmss", null);
+            switch (aliDateStr.Length)
+            {
+                case 10:
+                    return DateTime.ParseExact(aliDateStr, "yyyy-MM-dd", null);
+                    break;
+                default:
+                    return DateTime.ParseExact(aliDateStr.Substring(0, 14), "yyyyMMddHHmmss", null);
+                    break;
+            }
         }
 
         #endregion
