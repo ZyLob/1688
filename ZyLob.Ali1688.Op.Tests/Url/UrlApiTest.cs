0ec6007a4a8b6d522118bcf6643083b60f14fcdd
状元实验室 <2387979592@qq.com>
2015-08-19 18:13:46 +0800

    增加公共url接口单元测试

---
+++ ZyLob.Ali1688.Op.Tests/Url/UrlApiTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZyLob.Ali1688.Op.Context;
+
+namespace ZyLob.Ali1688.Op.Tests.Url
+{
+    [TestClass]
+    public class UrlApiTest : TestBase
+    {
+        [TestMethod]
+        public void GetBuyerOrderListUriTest()
+        {
+            var result = AliContext.Static.Url.GetBuyerOrderListUri();
+        }
+        [TestMethod]
+        public void GetSellerOrderListUriTest()
+        {
+            var result = AliContext.Static.Url.GetSellerOrderListUri();
+        }
+        [TestMethod]
+        public void GetOfferEditUriTest()
+        {
+            var result = AliContext.Static.Url.GetOfferEditUri(520525307433);
+        }
+    }
+}
