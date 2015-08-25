0e881c1cb3f0cb3e188b526d45d232d1c863b318
状元实验室 <2387979592@qq.com>
2015-08-25 18:10:03 +0800

    增加订单操作接口--未完

---
+++ ZyLob.Ali1688.Op/Trade/OrderApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZyLob.Ali1688.Op.Context;
+
+namespace ZyLob.Ali1688.Op.Trade
+{
+    /// <summary>
+    /// 订单操作接口
+    /// </summary>
+    public class OrderApi
+    {
+        private readonly AliContext _context;
+
+        public OrderApi(AliContext context)
+        {
+            _context = context;
+        }
+    }
+}
