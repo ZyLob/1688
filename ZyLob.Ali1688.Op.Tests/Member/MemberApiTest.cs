54098ed6a96d59ebe40f0508f29a32cd4e4d3717
状元实验室 <2387979592@qq.com>
2015-08-25 16:36:19 +0800

    增加会员接口单元测试

---
+++ ZyLob.Ali1688.Op.Tests/Member/MemberApiTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZyLob.Ali1688.Op.Context;
+
+namespace ZyLob.Ali1688.Op.Tests.Member
+{
+    [TestClass]
+    public class MemberApiTest
+    {
+        [TestMethod]
+        public void LoginIdToMemberIdTest()
+        {
+            var results = AliContext.Static.Member.LoginIdToMemberId("颖足鞋业", "缘顺食品", "聚美瑞工艺品");
+        }
+
+        [TestMethod]
+        public void MemberIdToLoginIdTest()
+        {
+            var results = AliContext.Static.Member.MemberIdToLoginId("b2b-1116603396", "b2b-1707661449", "b2b-2258587003");
+        }
+        [TestMethod]
+        public void GetMemberInfoTest()
+        {
+            var results = AliContext.Static.Member.GetMemberInfo("b2b-1116603396");
+        }
+           [TestMethod]
+        public void GetMemberIntegrityInfoTest()
+        {
+            var results = AliContext.Static.Member.GetMemberIntegrityInfo("b2b-1116603396", "b2b-1707661449", "b2b-2258587003");
+        }
+           [TestMethod]
+           public void GetAreaCodeTest()
+           {
+               var results = AliContext.Static.Member.GetAreaCode();
+           }
+    }
+}
