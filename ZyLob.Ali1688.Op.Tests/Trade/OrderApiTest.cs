4fe49beebd13cf9ecbf3436504edebfc2890473f
状元实验室 <2387979592@qq.com>
2015-08-28 18:28:00 +0800

    增加交易模块其余单元测试

--- ZyLob.Ali1688.Op.Tests/Trade/OrderApiTest.cs
+++ ZyLob.Ali1688.Op.Tests/Trade/OrderApiTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ZyLob.Ali1688.Op.Context;
 using ZyLob.Ali1688.Op.Models;
@@ -26,11 +27,46 @@ namespace ZyLob.Ali1688.Op.Tests.Trade
          {
              var results = AliContext.Static.Order.GetWholesaleSetting();
          }
+        [TestMethod]
+         public void TradeSearchTest()
+         {
+             var results = AliContext.Static.Order.TradeSearch(new TradeSeachModel()
+             {
+                 OrderStatus = TradeStatus.success,
+                 SellerMemberId = "b2b-1623492085"
+             });
+         }
           [TestMethod]
          public void GetTradeDetailTest()
          {
              var results = AliContext.Static.Order.GetTradeDetail(1170259893000799);
          }
+           [TestMethod]
+          public void OrderEvaluateTest()
+           {
+               var paras = new Dictionary<long, OrderEvaluateModel[]>();
+               paras.Add(1170259893000799,new OrderEvaluateModel[]{ new OrderEvaluateModel()
+               {
+                   Content = "好",
+                   StarLevel = 5
+               }});
+               paras.Add(1145809440038108,new OrderEvaluateModel[]{  new OrderEvaluateModel()
+               {
+                   Content = "好",
+                   StarLevel = 5
+               }});
+               var results = AliContext.Static.Order.OrderEvaluate(paras);
+         }
+          [TestMethod]
+         public void ModifyOrderPriceTest()
+         {
+             var results = AliContext.Static.Order.ModifyOrderPrice(998161929759811, 200, new ModifyOrderPriceDetail[]{new ModifyOrderPriceDetail()
+             {
+                 Id =998161929759811,
+                 Discount = 20
+             }});
+         }
+        
         
     }
     
