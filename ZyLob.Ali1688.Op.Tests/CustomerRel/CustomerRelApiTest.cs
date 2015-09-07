559ed6239b8af2113ad29d6ac72bfbc2f5db65fb
状元实验室 <2387979592@qq.com>
2015-09-07 15:27:21 +0800

    新增部分客户关系接口单元测试

---
+++ ZyLob.Ali1688.Op.Tests/CustomerRel/CustomerRelApiTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZyLob.Ali1688.Op.Context;
+using ZyLob.Ali1688.Op.Models;
+
+namespace ZyLob.Ali1688.Op.Tests.CustomerRel
+{
+    [TestClass]
+    public class CustomerRelApiTest:TestBase
+    {
+        [TestMethod]
+        public void GetCustomerByTradeTest()
+        {
+            AliContext.Static.CustomerRelations.GetCustomerByTrade();
+        }
+        [TestMethod]
+        public void GetCustomerByGroupTest()
+        {
+            AliContext.Static.CustomerRelations.GetCustomerByGroup(350004269);
+        }
+        [TestMethod]
+        public void GetCustomerByLevelTest()
+        {
+            AliContext.Static.CustomerRelations.GetCustomerByLevel(CustomerLevel.所有的);
+        }
+        [TestMethod]
+        public void GetGroupsTest()
+        {
+            AliContext.Static.CustomerRelations.GetGroups();
+        }
+    }
+}
