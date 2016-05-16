596d9d14c33a9d0719be12aad13959b37c42c85a
状元实验室 <2387979592@qq.com>
2016-05-16 16:06:12 +0800

    独立统计内容

---
+++ ZyLob.Ali1688.Op/Common/ApiStatistics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZyLob.Ali1688.Op.Common
+{
+    /// <summary>
+    /// 接口统计
+    /// </summary>
+  public  static class ApiStatistics
+    {
+        static ApiStatistics()
+        {
+            SendCounts = new Dictionary<string, long>();
+        }
+        /// <summary>
+        /// 成功发送请求数
+        /// </summary>
+        public static Dictionary<string,long> SendCounts { get;private set; }
+        /// <summary>
+        /// 发送记录
+        /// </summary>
+        internal static void SendRecord()
+        {
+            var nowDate = DateTime.Now.ToString("yyyy-MM-dd");
+            if (!SendCounts.ContainsKey(nowDate))
+            {
+                SendCounts.Add(nowDate,0);
+            }
+            SendCounts[nowDate]++;
+        }
+    }
+}
